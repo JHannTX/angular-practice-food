f6388285a85c85dd14d9139155d229c618b98ea0
JHann <jhann@tamu.edu>
2023-06-06 18:39:57 -0500

    Added meals url constants

--- src/app/constants/service-constants.ts
+++ src/app/constants/service-constants.ts
@@ -8,11 +8,11 @@ export class ServiceConstants {
 
     public COCKTAILS_URL = {
         BASE_COCKTAILS_URL: 'http://www.thecocktaildb.com/api/json/v1/1/',
-        SEARCH_COCKTAILS_URL: 'search.php',
-        LOOKUP_COCKTAILS_URL: 'lookup.php',
+        SEARCH_COCKTAILS_URL: 'search.php?',
+        LOOKUP_COCKTAILS_URL: 'lookup.php?',
         RANDOM_COCKTAILS_URL: 'random.php',
-        FILTER_COCKTAILS_URL: 'filter.php',
-        LIST_COCKTAILS_URL: 'list.php'
+        FILTER_COCKTAILS_URL: 'filter.php?',
+        LIST_COCKTAILS_URL: 'list.php?'
     };
 
     public FRUITS_URL = {
@@ -21,5 +21,14 @@ export class ServiceConstants {
         FAMILY_FRUITS_URL: 'family/',
         GENUS_FRUITS_URL: 'genus/',
         ORDER_FRUITS_URL: 'order/',
+    };
+
+    public MEALS_URL = {
+        BASE_MEALS_URL: 'www.themealdb.com/api/json/v1/1/',
+        SEARCH_MEALS_URL: 'search.php?',
+        LOOKUP_MEALS_URL: 'lookup.php?i=',
+        RANDOM_MEALS_URL: 'random.php',
+        LIST_MEALS_URL: 'list.php?',
+        FILTER_MEALS_URL: 'filter.php?',
     };
 }
