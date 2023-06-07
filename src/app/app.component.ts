f4069d9ae07d8ef3b62d024c7b18163755c175b7
JHann <jhann@tamu.edu>
2023-06-07 14:37:41 -0500

    changed the selector and added in api testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { CocktailsService } from './services/cocktails.service';
+import { FruitsService } from './services/fruits.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'fd-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'food';
+
+  constructor(private cocktailsService: CocktailsService, private fruitsService: FruitsService) {}
+
+  ngOnInit() {
+    this.cocktailsService.search('name', 'margarita')
+      .subscribe(response => {
+        console.log(response);
+      });
+
+      this.fruitsService.all()
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
